a4304aa8b0f579ab3ef04775e95bf3401fbbb734
StarKujo <57246348+StarKujo@users.noreply.github.com>
2026-08-28 13:21:02 +0800

    Initial public release source

---
+++ addon/bootstrap.js
@@ -0,0 +1,36 @@
+var chromeHandle;
+
+function install() {}
+
+async function startup({ rootURI, resourceURI }, reason) {
+  await Zotero.initializationPromise;
+  rootURI ||= resourceURI.spec;
+  const aomStartup = Cc["@mozilla.org/addons/addon-manager-startup;1"].getService(
+    Ci.amIAddonManagerStartup,
+  );
+  chromeHandle = aomStartup.registerChrome(Services.io.newURI(rootURI + "manifest.json"), [
+    ["content", "zoterowordbook", rootURI + "chrome/content/"],
+  ]);
+  const context = { rootURI, Zotero, Services, Cc, Ci, ChromeUtils };
+  context._globalThis = context;
+  Services.scriptloader.loadSubScript(
+    rootURI + "chrome/content/scripts/zoterowordbook.js",
+    context,
+  );
+}
+
+async function onMainWindowLoad({ window }) {
+  await Zotero.ZoteroWordbook?.hooks.onMainWindowLoad(window);
+}
+
+async function onMainWindowUnload({ window }) {
+  await Zotero.ZoteroWordbook?.hooks.onMainWindowUnload(window);
+}
+
+async function shutdown(data, reason) {
+  if (reason !== APP_SHUTDOWN) await Zotero.ZoteroWordbook?.hooks.onShutdown();
+  chromeHandle?.destruct();
+  chromeHandle = null;
+}
+
+function uninstall() {}
